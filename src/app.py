66eb1e28cc12a5e57d39c67928f03a24b709973c
makhosandile <mpnyamela@gmail.com>
2023-10-21 14:38:37 +0200

    Added helth

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,7 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
+@app.route("/health")
+def health():
+    return jsonify(
+        status="UP"
+    )
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
