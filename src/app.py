c7820ab82cf387dd5dd6d73545576e3a6e11cb5b
makhosandile <mpnyamela@gmail.com>
2023-10-21 15:46:10 +0200

    Added detils rout

--- src/app.py
+++ src/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, render_template
+import socket
 app = Flask(__name__)
+
+# Function to fetch hostname and ip 
+def fetchDetails():
+    hostname = socket.gethostname()
+    host_ip = socket.gethostbyname(hostname)
+    return str(hostname), str(host_ip)
 
 @app.route("/")
 def hello_world():
@@ -11,6 +17,10 @@ def health():
     return jsonify(
         status="UP"
     )
+@app.route("/details")
+def details():
+    hostname, ip = fetchDetails()
+    return render_template('index.html', HOSTNAME=hostname, IP=ip)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
